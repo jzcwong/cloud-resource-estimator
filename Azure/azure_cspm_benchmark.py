3872f30ee33bf59277bfe6a79a5fab5230f087c0
chianw <chiancw@gmail.com>
2026-07-27 15:13:32 +0800

    update resource provider

--- Azure/azure_cspm_benchmark.py
+++ Azure/azure_cspm_benchmark.py
@@ -17,7 +17,7 @@ import os
 
 from functools import cached_property, lru_cache
 from azure.identity import AzureCliCredential
-from azure.mgmt.resource import ResourceManagementClient
+from azure.mgmt.resource.resources import ResourceManagementClient
 from azure.mgmt.resource.subscriptions import SubscriptionClient
 from azure.mgmt.containerservice import ContainerServiceClient
 from azure.mgmt.compute import ComputeManagementClient
